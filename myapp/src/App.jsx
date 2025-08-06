ed971b02223daff7eba2a71956c54388514982cd
PuneethKumar M S <puneethkumarms912@gmail.com>
2025-08-06 20:38:08 +0530

    todo

---
+++ myapp/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ToDoFunction from "./components/ToDoFunction";
+
+function App() {
+  return(
+    <div className="app-container">
+      <h1>To-Do-List</h1>
+      <ToDoFunction/>
+
+    </div>
+  );
+}
+export default App;
---
+++ myapp/src/components/ToDoFunction.jsx
@@ -0,0 +1,59 @@
+import React, {useState} from "react";
+
+function ToDoFunction() {
+
+    const[tasks, setTasks] = useState([]);
+    const[task, setTask] = useState("");
+
+    const addTask = () => {
+
+        if(task.trim() !=="") {
+
+            setTasks([...tasks, {text: task, completed: false}]);
+            setTask("");
+
+        }
+    };
+
+    const toggleTask = (index) => {
+
+        const updatedTasks = [...tasks];
+        updatedTasks[index].completed = !updatedTasks[index].completed;
+        setTasks(updatedTasks);
+
+    };
+
+    const deleteTask = (index) => {
+
+        const updatedTasks = tasks.filter((_, i) => i!==index);
+        setTasks(updatedTasks);
+    };
+
+    return(
+        <div className="todo-container">
+            <input
+            type="text"
+            value={task}
+            placeholder="Type Something..."
+            onChange={(e) => setTask(e.target.value)}
+            />
+
+            <button onClick={addTask}>Add Task</button>
+
+            <ul>
+                {tasks.map((t, index) => (
+
+                    <li key={index} className={t.completed ? "completed": ""}>
+                        <span onClick={()=> toggleTask(index)}>{t.text}</span>
+                        <button className="delete-btn" onClick={()=> deleteTask(index)}>Delete</button>
+                    </li>
+
+                ))}
+
+
+            </ul>
+
+        </div>
+    );
+}
+export default ToDoFunction;
