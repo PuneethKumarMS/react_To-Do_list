c63afc72f773d81a7332dc39db3b5992eb07a69f
PuneethKumar M S <puneethkumarms912@gmail.com>
2026-01-13 12:02:30 +0530

    Update main.jsx

--- myapp/src/main.jsx
+++ myapp/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import './index.css' // css for styles
+import App from './App.jsx' //file import
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
