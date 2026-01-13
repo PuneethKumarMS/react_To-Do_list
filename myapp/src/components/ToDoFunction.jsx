e69bd58d35353ae27644f36518e4d3115e874fda
PuneethKumar M S <puneethkumarms912@gmail.com>
2026-01-13 12:01:26 +0530

    Update ToDoFunction.jsx

--- myapp/src/components/ToDoFunction.jsx
+++ myapp/src/components/ToDoFunction.jsx
@@ -1,30 +1,23 @@
 import React, {useState} from "react";
 
 function ToDoFunction() {
-
     const[tasks, setTasks] = useState([]);
     const[task, setTask] = useState("");
 
     const addTask = () => {
-
         if(task.trim() !=="") {
-
             setTasks([...tasks, {text: task, completed: false}]);
             setTask("");
-
         }
     };
 
     const toggleTask = (index) => {
-
         const updatedTasks = [...tasks];
         updatedTasks[index].completed = !updatedTasks[index].completed;
         setTasks(updatedTasks);
-
     };
 
     const deleteTask = (index) => {
-
         const updatedTasks = tasks.filter((_, i) => i!==index);
         setTasks(updatedTasks);
     };
@@ -39,20 +32,14 @@ function ToDoFunction() {
             />
 
             <button onClick={addTask}>Add Task</button>
-
             <ul>
                 {tasks.map((t, index) => (
-
                     <li key={index} className={t.completed ? "completed": ""}>
                         <span onClick={()=> toggleTask(index)}>{t.text}</span>
                         <button className="delete-btn" onClick={()=> deleteTask(index)}>Delete</button>
                     </li>
-
                 ))}
-
-
             </ul>
-
         </div>
     );
 }
